85057fdd6ab66b130e8953df0e4e4b15f50b37cd
Armin Ruech <armin.ruech@dynatrace.com>
2019-06-17 09:53:28 +0200

    add incoming web request tracer bump SDK version to 1.5.0

---
+++ src/DummyImpl/DummyIncomingWebRequestTracer.cs
@@ -0,0 +1,65 @@
+//
+// Copyright 2019 Dynatrace LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System;
+using System.Threading.Tasks;
+
+namespace Dynatrace.OneAgent.Sdk.Api.DummyImpl
+{
+    internal class DummyIncomingWebRequestTracer : IIncomingWebRequestTracer
+    {
+        public void Start() { }
+
+        public Task StartAsync() => Task.FromResult(0);
+
+        public void Error(string message) { }
+
+        public void Error(Exception exception) { }
+
+        public void End() { }
+
+        public void Trace(Action action) => action?.Invoke();
+
+        public T Trace<T>(Func<T> func)
+        {
+            return func != null ? func() : default(T);
+        }
+
+        public Task TraceAsync(Func<Task> func)
+        {
+            return func != null ? func() : null;
+        }
+
+        public Task<T> TraceAsync<T>(Func<Task<T>> func)
+        {
+            return func != null ? func() : null;
+        }
+
+        public void SetDynatraceByteTag(byte[] tag) { }
+
+        public void SetDynatraceStringTag(string tag) { }
+
+        public void SetRemoteAddress(string remoteAddress) { }
+
+        public void AddParameter(string name, string value) { }
+
+        public void AddRequestHeader(string name, string value) { }
+
+        public void AddResponseHeader(string name, string value) { }
+
+        public void SetStatusCode(int statusCode) { }
+    }
+}
